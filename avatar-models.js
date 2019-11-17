924ee823e9f38e4c01a5d535638602d254fd23ed
Avaer Kazmer <a@modules.io>
2019-11-16 19:44:23 -0500

    Update avatar-models.js again

--- avatar-models.js
+++ avatar-models.js
@@ -1,147 +1,147 @@
-export default JSON.parse(`"[
+export default JSON.parse(`[
   {
-    "label": "",
+    "label": "model.glb",
     "url": "model.glb",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model2.vrm",
     "url": "model2.vrm",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model3.unitypackage",
     "url": "model3.unitypackage",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model4.zip",
     "url": "model4.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model5.fbx",
     "url": "model5.fbx",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model6.zip",
     "url": "model6.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model7.vrm",
     "url": "model7.vrm",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model8.vrm",
     "url": "model8.vrm",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model9.vrm",
     "url": "model9.vrm",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model10.glb",
     "url": "model10.glb",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model10.vrm",
     "url": "model10.vrm",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model11.vrm",
     "url": "model11.vrm",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model12.fbx",
     "url": "model12.fbx",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model13.zip",
     "url": "model13.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model14.zip",
     "url": "model14.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model15.zip",
     "url": "model15.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model16.zip",
     "url": "model16.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model17.zip",
     "url": "model17.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model18.zip",
     "url": "model18.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model19.zip",
     "url": "model19.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model20.zip",
     "url": "model20.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model21.zip",
     "url": "model21.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model22.zip",
     "url": "model22.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model23.zip",
     "url": "model23.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model24.zip",
     "url": "model24.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model25.zip",
     "url": "model25.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model26.zip",
     "url": "model26.zip",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model27.png",
     "url": "model27.png",
     "icon": ""
   },
   {
-    "label": "",
+    "label": "model28.png",
     "url": "model28.png",
     "icon": ""
   }
-]"`)
+]`)
